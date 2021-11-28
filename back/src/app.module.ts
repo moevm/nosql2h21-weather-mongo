fa7328d11afb014837882a0cc98da7cdf1866e45
Egor Senushkin <senushkinev@yandex.ru>
2021-11-28 19:31:11 +0300

    Add docker.

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -6,7 +6,7 @@ import { WeatherModule } from './weather/weather.module';
 import { RegionModule } from './regions/regions.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/weatherdb'),
+  imports: [MongooseModule.forRoot('mongodb://mongodb/weatherdb'),
             WeatherModule,
             RegionModule],
   controllers: [AppController],
